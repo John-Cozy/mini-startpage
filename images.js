0658462c75d4fe9353b61d5237a3c3b445df35ad
Sir-Shrekington <s1178740@gmail.com>
2019-01-24 20:29:35 +0000

    Refactoring JS

--- images.js
+++ images.js
@@ -1,11 +1,13 @@
-var cur;
+var num;
 
-function displayImage(page, num){
-  num = Math.floor(Math.random() * num);
-  cur = num;
-  document.getElementById('image_box').style.backgroundImage="url(../images/" + page + "/" + (num + 1) + ".png)";
+function displayImage(page, max){
+  num = Math.floor(Math.random() * max);
+  setImage(page);
 }
 function swapImage(page, max){
-  cur == max - 1 ? cur = 0 : cur++;
-  document.getElementById('image_box').style.backgroundImage="url(../images/" + page + "/" + (cur + 1) + ".png)";
+  num == max - 1 ? num = 0 : num++;
+  setImage(page);
+}
+function setImage(page){
+  document.getElementById('image_box').style.backgroundImage="url(../images/" + page + "/" + (num + 1) + ".png)";
 }
