e760f8e067a70ad76bc975fc476ff654eb1e6f95
Sir-Shrekington <s1178740@gmail.com>
2019-01-24 19:29:24 +0000

    Added swap image button

--- images.js
+++ images.js
@@ -1,4 +1,11 @@
+var cur;
+
 function displayImage(page, num){
-    num = Math.floor(Math.random() * num);
-    document.getElementById('image_box').style.backgroundImage="url(../images/" + page + "/" + (num + 1) + ".png)";
+  num = Math.floor(Math.random() * num);
+  cur = num;
+  document.getElementById('image_box').style.backgroundImage="url(../images/" + page + "/" + (num + 1) + ".png)";
+}
+function swapImage(page, max){
+  cur == max - 1 ? cur = 0 : cur++;
+  document.getElementById('image_box').style.backgroundImage="url(../images/" + page + "/" + (cur + 1) + ".png)";
 }
